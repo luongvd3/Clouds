b148fce9ee794ddf5deba5a039709230989d37ce
Luong <luongvd3@gmail.com>
2022-03-24 02:44:28 +1100

    update presentaion

--- Main.py
+++ Main.py
@@ -44,11 +44,7 @@ def isWithin(point, boundary, position):
         return False
 
 def merge(source, destination):
-    """
-    run me with nosetests --with-doctest file.py
 
-    True
-    """
     for key, value in source.items():
         if isinstance(value, dict):
             # get node or create one
@@ -63,6 +59,7 @@ tweetFilePath = "bigTwitter.json"
 try:
     tweetStream = io.open(tweetFilePath, "r", encoding="utf-8")
     gridsJson = json.load(open("sydGrid-2.json", "r"))
+    languages = [line[:-1].split(" ") for line in open("languages.txt", "r", encoding="utf-8").readlines()]
 except IOError as e:
     print("Tweet file not found")
     quit()
@@ -76,23 +73,30 @@ for grid in gridsJson['features']:
     leftRight = [min(boundary[0]), max(boundary[0])]
     topBottom = [max(boundary[1]), min(boundary[1])]
     processedGrids.append([leftRight, topBottom])
-
+languagesDict = {}
+for line in languages:
+    languagesDict[line[1]] = line[0]
+print(languagesDict)
 print(processedGrids)
 #Skip first row
 next(tweetStream)
 languageCount = collections.defaultdict(partial(collections.defaultdict, int))
-for i in range(5000000):
+for i in range(5000):
     #print(next(tweetStream)[:-2])
     try:
         tweetStr = next(tweetStream)[:-2];
         tweetStr = tweetStr[:-1] if tweetStr[-1] == "]" else tweetStr
         try:
             tweet = json.loads(tweetStr)
-            if tweet['doc']['coordinates']:
+            if tweet['doc']['coordinates'] and tweet['doc']['lang'] != "und" and tweet['doc']['lang'] != "null":
                 # print(tweet['doc']['coordinates']['coordinates'])
                 for boundary in processedGrids:
                     if isWithin(tweet['doc']['coordinates']['coordinates'], boundary, gridPosition[processedGrids.index(boundary)]):
-                        languageCount[processedGrids.index(boundary)+1][tweet['doc']['lang']] += 1
+                        if tweet['doc']['lang'] in languagesDict.keys():
+                            languageCount[processedGrids.index(boundary)+1][languagesDict[tweet['doc']['lang']]] += 1
+                        else:
+                            languageCount[processedGrids.index(boundary)+1][tweet['doc']['lang']] += 1
+
         except json.decoder.JSONDecodeError:
             print(tweetStr)
         #print(tweet)
@@ -101,6 +105,19 @@ for i in range(5000000):
         print("End of file")
         break
 print("--- %s seconds ---" % (time.time() - start_time))
+languageCount = dict(sorted(languageCount.items(), key=lambda item: len(item[1])))
+final_result = {}
+for region, data in languageCount.items():
+    data = dict(sorted(data.items(), key=lambda item: item[1]))
+    final_result[region] = {}
+    final_result[region]["Total Tweet"] = sum(data.values())
+    final_result[region]["Number of Languages Used"] = len(data)
+    top10 = ""
+    i = 0
+    while i < len(data) and i < 10:
+        top10 += " "+ list(data.keys())[i] + "-" + str(list(data.values())[i])
+        i += 1
+    final_result[region]["Top 10 Languages & #Tweets"] = top10
 #Test
 for pair in processedGrids:
     assert pair[0][0] < pair[0][1]
@@ -109,3 +126,4 @@ a = collections.defaultdict(partial(collections.defaultdict, int))
 a['1']['es'] = 10
 # languageCount['1']['es'] += 10
 print(merge(a,languageCount))
+print(final_result)
--- MpiMain.py
+++ MpiMain.py
@@ -5,7 +5,10 @@ from mpi4py import MPI
 import numpy as np
 from functools import partial
 
-gridPosition = ["topLeft", "left", "left", "bottomLeft","top","center","center","bottom","top","center","center","bottom","topRight","right","right","bottomRight"]
+gridPosition = ["topLeft", "left", "left", "bottomLeft", "top", "center", "center", "bottom", "top", "center", "center",
+                "bottom", "topRight", "right", "right", "bottomRight"]
+
+
 # decide if a coordinates is within a region following all the conditions set out in the assignment specs
 # note that the grids coordinates have more numbers behind the floating point or  are more details than the ones from the tweets so it's not
 # possible a tweet to fall on a border of a grid, so the extra conditions seem unnecessary
@@ -40,6 +43,8 @@ def isWithin(point, boundary, position):
             return True
     else:
         return False
+
+
 # Credit stackoverflow
 def merge(source, destination):
     for key, value in source.items():
@@ -52,6 +57,7 @@ def merge(source, destination):
 
     return destination
 
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 print(rank)
@@ -59,17 +65,17 @@ start_time = 0
 if rank == 0:
     start_time = time.time()
 
-
 tweetFilePath = "bigTwitter.json"
 try:
     tweetStream = open(tweetFilePath, "r", encoding="utf-8")
     gridsJson = json.load(open("sydGrid-2.json", "r"))
+    languages = [line[:-1].split(" ") for line in open("languages.txt", "r", encoding="utf-8").readlines()]
 except IOError as e:
-    print("Tweet file not found")
+    print("File not found")
     quit()
 
 processedGrids = [];
-#get boundaries of each square
+# get boundaries of each square
 for grid in gridsJson['features']:
     boundary = np.array(grid['geometry']['coordinates'][0]).T
     leftRight = [min(boundary[0]), max(boundary[0])]
@@ -77,18 +83,21 @@ for grid in gridsJson['features']:
     processedGrids.append([leftRight, topBottom])
 
 # print(processedGrids)
+languagesDict = {}
+for line in languages:
+    languagesDict[line[1]] = line[0]
 
 batch_size = 250
-endOfFile= False
+endOfFile = False
 
-#Skip first row
+# Skip first row
 next(tweetStream)
 languageCount = collections.defaultdict(partial(collections.defaultdict, int))
 while not endOfFile:
     # skip rows that are being processed by others
     for j in range(batch_size * rank):
         try:
-            #if rank == 1: print(rank,j)
+            # if rank == 1: print(rank,j)
             next(tweetStream)
         except StopIteration:
             endOfFile = True
@@ -96,18 +105,21 @@ while not endOfFile:
             print("End of file")
 
     for i in range(batch_size):
-        #print(next(tweetStream)[:-2])
+        # print(next(tweetStream)[:-2])
         try:
             # print(rank,i)
             tweetStr = next(tweetStream)[:-2];
             tweetStr = tweetStr[:-1] if tweetStr[-1] == "]" else tweetStr
             try:
                 tweet = json.loads(tweetStr)
-                if tweet['doc']['coordinates']:
+                if tweet['doc']['coordinates'] and tweet['doc']['lang'] != "und" and tweet['doc']['lang'] != "null":
                     # print(tweet['doc']['coordinates']['coordinates'])
                     for boundary in processedGrids:
                         if isWithin(tweet['doc']['coordinates']['coordinates'], boundary, gridPosition[processedGrids.index(boundary)]):
-                            languageCount[processedGrids.index(boundary)+1][tweet['doc']['lang']] += 1
+                            if tweet['doc']['lang'] in languagesDict.keys():
+                                languageCount[processedGrids.index(boundary)+1][languagesDict[tweet['doc']['lang']]] += 1
+                            else:
+                                languageCount[processedGrids.index(boundary)+1][tweet['doc']['lang']] += 1
             except ValueError:
                 print("Unable to decode: ", tweetStr)
         except StopIteration:
@@ -123,13 +135,28 @@ while not endOfFile:
             endOfFile = True
             break
             print("End of file")
-print("Local result is ", json.dumps(languageCount))
+# print("Local result is ", json.dumps(languageCount))
 languageCount = json.dumps(languageCount)
-gatheredLanguageCount = comm.gather(languageCount,0)
+gatheredLanguageCount = comm.gather(languageCount, 0)
 if rank == 0:
-    final_result= collections.defaultdict(partial(collections.defaultdict, int))
+    gathered_result = collections.defaultdict(partial(collections.defaultdict, int))
     # print(str(gatheredLanguageCount))
     for i in range(comm.size):
-        final_result = merge(json.loads(gatheredLanguageCount[i]),final_result)
-    print("Final result is: ",json.dumps(final_result))
+        gathered_result = merge(json.loads(gatheredLanguageCount[i]), gathered_result)
+
+    gathered_result = dict(sorted(gathered_result.items(), key=lambda item: len(item[1]), reverse=True))
+
+    final_result = {}
+    for region, data in gathered_result.items():
+        data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
+        final_result[region] = {}
+        final_result[region]["Total Tweet"] = sum(data.values())
+        final_result[region]["Number of Languages Used"] = len(data)
+        top10 = ""
+        i = 0
+        while i < len(data) and i < 10:
+            top10 += " "+ list(data.keys())[i] + "-" + str(list(data.values())[i])
+            i += 1
+        final_result[region]["Top 10 Languages & #Tweets"] = top10
+    print("Final result is: ", json.dumps(final_result))
     print("--- %s seconds ---" % (time.time() - start_time))
